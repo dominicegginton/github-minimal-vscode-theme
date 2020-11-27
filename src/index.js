7a31e8ee808dc53dcc70663d6ad845dd43463edf
Dominic Egginton <dominic.egginton@gmail.com>
2020-11-27 16:10:37 +0000

    created minimal dark theme

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 const theme = require('./theme')
 
 const lightTheme = theme({ name: 'GitHub Minimal', style: 'light' })
+const darkTheme = theme({ name: 'GitHub Minimal', style: 'dark' })
 
 fs.mkdirSync('./themes', { recursive: true })
 fs.writeFileSync('./themes/light.json', JSON.stringify(lightTheme))
+fs.writeFileSync('./themes/dark.json', JSON.stringify(darkTheme))
